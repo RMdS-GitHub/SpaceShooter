35dc4af0a6f775e852b13e0525ba7bd129e7070c
RMdS-GitHub <rmds.dev@protonmail.com>
2020-11-30 15:49:06 +0100

    Rigid bullets and instancing. Creation of the bullet.tscn. In the Ship.gd we called the bullet as a child of the world node when getting input from the player. When we play the game and check the remote we can see that the bullets and enemies object are still on even when not in the screen.

--- SpaceShooter/Ship.gd
+++ SpaceShooter/Ship.gd
@@ -1,10 +1,13 @@
 """
 Ship.gd
-Extends Area2D, contains, var SPEED, _process(). 
+Extends Area2D, contains, var SPEED, const BULLET, _process(), fire_bullet(). 
 This controls the ship movement, which goes up and down.
 
 """
 extends Area2D
+
+# This const gives access to bullet.tscn.
+const BULLET = preload("res://Bullet.tscn")
 
 # Variable to be used in _process() for movement.
 export(int) var SPEED = 100
@@ -12,10 +15,28 @@ export(int) var SPEED = 100
 
 """
 func _process()
-Receives input from player, move ship up and down.
+Receives input from player, move ship up and down holding the key.
+Fires the bullets just pressing the key. 
 """ 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_up"):
 		position.y -= SPEED * delta
 	if Input.is_action_pressed("ui_down"):
 		position.y += SPEED * delta
+	if Input.is_action_just_pressed("ui_accept"):
+		fire_bullet()
+
+
+"""
+func fire_bullet()
+Gets the bullet.tsnc with var bullet, gets the World node (root) with main.
+
+With these two var declared it adds the bullet as child in the World node. 
+
+Sets the bullet position to the position of the ship.
+"""
+func fire_bullet():
+	var bullet = BULLET.instance()
+	var main = get_tree().current_scene
+	main.add_child(bullet)
+	bullet.global_position = global_position
